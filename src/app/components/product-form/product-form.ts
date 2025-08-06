c4383560c6f60c5a3935ecd231a2344700962b49
Hemerson Ballen <hjballent@gmail.com>
2025-08-06 18:05:37 -0500

    Added the injection of the ProductApi Service

--- src/app/components/product-form/product-form.ts
+++ src/app/components/product-form/product-form.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { ProductsApi } from '@app/core/services/products-api';
 
 @Component({
   selector: 'app-product-form',
@@ -8,10 +9,17 @@ import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
   styleUrl: './product-form.css'
 })
 export class ProductForm {
+
+  private readonly produtcoService =  inject(ProductsApi)
+
   registerForm = new FormGroup({
     nombre: new FormControl(""),
     imagen: new FormControl(File),
     descripcion: new FormControl(""),
     categoria: new FormControl(""),
   })
+
+  submitProducto(){
+    this.produtcoService.postProduct()
+  }
 }
